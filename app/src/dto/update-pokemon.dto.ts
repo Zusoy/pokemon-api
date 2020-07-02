63acb1bb937034481973e501044c2d497534c807
Greg <gregoire.drapeau79@gmail.com>
2020-07-02 15:13:11 +0200

    Added Swagger Docs

--- app/src/dto/update-pokemon.dto.ts
+++ app/src/dto/update-pokemon.dto.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { PokemonType } from "src/entity/pokemon.type.enum";
+
 export class UpdatePokemonDto {
+    @ApiProperty({description: 'The name of the Pokemon'})
     name: string;
+
+    @ApiProperty({description: 'The Type of the Pokemon', enum: PokemonType})
     type: string;
 }
