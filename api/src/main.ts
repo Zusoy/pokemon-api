9b1de1c61242d2d329605347fdcbdde174b420b5
Greg <gregoire.drapeau79@gmail.com>
2020-07-09 16:25:18 +0200

    Addec CORS for API

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
 
   const options = new DocumentBuilder()
     .setTitle('Pokemon')
@@ -14,7 +16,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('doc', app, document);
-
+  
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
