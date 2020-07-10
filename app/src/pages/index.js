a5190de606c943023f011b9a57395601e95c5438
Greg <gregoire.drapeau79@gmail.com>
2020-07-10 14:58:45 +0200

    Deleted added pages

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,6 +1,4 @@
 export { default as Home } from './Home';
-export { default as AddTrainer } from './AddTrainer';
-export { default as AddPokemon } from './AddPokemon';
 export { default as ListHuman } from './ListHuman';
 export { default as ListPokemon } from './ListPokemon';
 export { default as ListBox } from './ListBox';
